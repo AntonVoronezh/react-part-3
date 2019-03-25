eeb979efce22b09bfd2412ed8318af2d8264d49f
AntonVoronezh <pstut@mail.ru>
2019-03-25 21:15:44 +0300

    8#2

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
 import React, { Component } from "react";
 import classes from "./App.scss";
-import Cars from './Cars/Cars.jsx'
-import About from './About/About.jsx'
-import {Route, NavLink} from 'react-router-dom'
+import Cars from "./Cars/Cars.jsx";
+import About from "./About/About.jsx";
+import { Route, NavLink, Switch } from "react-router-dom";
+import CarDetail from "./CarDetail/CarDetail.jsx";
 
 class App extends Component {
-
   render() {
-    
     return (
       <div>
         <nav className={classes.nav}>
           <ul>
             <li>
-              <NavLink to="/" exact>Home</NavLink>
+              <NavLink to="/" exact>
+                Home
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/about" activeStyle={{color:'blue',fontWeight:'bold'}}>About</NavLink>
+              <NavLink
+                to="/about"
+                activeStyle={{ color: "blue", fontWeight: "bold" }}
+              >
+                About
+              </NavLink>
             </li>
             <li>
-              <NavLink to={{
-                pathname:"/cars"
-              }}>Cars</NavLink>
+              <NavLink
+                to={{
+                  pathname: "/cars"
+                }}
+              >
+                Cars
+              </NavLink>
             </li>
           </ul>
         </nav>
-       
-         <Route path="/" exact render={()=><h1>Home page</h1>} />
-         <Route path="/about" exact component={About} />
-         <Route path="/cars" exact component={Cars} />
+          <Route path="/" exact render={() => <h1>Home page</h1>} />
+          <Route path="/about" exact component={About} />
+          <Route path="/cars" component={Cars} />
       </div>
     );
   }
